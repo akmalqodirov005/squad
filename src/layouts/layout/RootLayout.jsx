14ce86f0035be422c599bd09c120a4f0411d0dff
akmalqodirov005 <akmalqodirov005@gmail.com>
2026-07-01 16:18:02 +0500

    aa

--- src/layouts/layout/RootLayout.jsx
+++ src/layouts/layout/RootLayout.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from '../footer/Footer'
 import Header from '../header/Header'
+import './RootLayout.css'
 
 const RootLayout = () => {
   return (
@@ -10,7 +10,7 @@ const RootLayout = () => {
             <Header/>
         </header>
 
-        <main>
+        <main className='main'>
             <Outlet/>
         </main>
 
